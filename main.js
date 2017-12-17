08aa3f447c8ad6d31128ec5abcec2c4d710cb037
martinproxy0 <marcinsornek@gmail.com>
2017-12-17 19:26:55 +0100

    Add new prototype method and exercise code

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var brands = ['Nokia', 'BalckBerry', 'Samsung'],
-    colors = ['black', 'white', 'grey'];
+    colors = ['black', 'white', 'grey'],
+    myPhones = [];
 
 function Phone(brand, price, color) {
     this.brand = brand;
@@ -8,24 +9,62 @@ function Phone(brand, price, color) {
 }
 
 Phone.prototype.printInfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+    return ("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 };
 Phone.prototype.ringing = function() {
-    console.log("Your's " + this.color + " phone is ringing now! Pick up " + this.brand);
+    return ("Your's " + this.color + " phone is ringing now! Pick up " + this.brand);
 };
 
-function makePhone(brand, price, color) {
-    return new Phone(brand, price, color);
+(function() {
+    initialize();
+})();
+
+function initialize() {
+    makePhones();
+    addParagraph(myPhones);
+    document.getElementById("btn-go").addEventListener("click", chooseRingingPhone);
 }
 
 function makePhones() {
-    brands.forEach(function(element, index) {
-        var price = Math.random() * 100;
-        makePhone(element, price, colors[index]);
-    });
-    return phones;
+    for (let i = 0; i < 3; i++) {
+        myPhones[i] = makePhone(i, 1000, i);
+    }
 }
 
-var myPhones = [];
-myPhones = makePhones();
-console.log(myPhones[0]);
+function makePhone(brand, price, color) {
+    var priceCalc = Math.round(Math.random() * price);
+    return new Phone(brands[brand], priceCalc, colors[color]);
+}
+
+function addParagraph(phones) {
+    phones.forEach(function(element) {
+        var paragraph = document.createElement('p'),
+            myDiv = document.getElementById('phones');
+        paragraph.innerHTML = element.printInfo();
+        document.getElementById('phones').appendChild(paragraph);
+    });
+}
+
+function chooseRingingPhone() {
+    var variant = Math.round(Math.random() * 2);
+    ringingPhone(myPhones[variant]);
+
+}
+
+function ringingPhone(phone) {
+    var h1 = document.createElement('h1');
+    h1.innerHTML = phone.ringing();
+    h1.className = phone.brand;
+    h1.id = 'ringing';
+    document.body.appendChild(h1);
+    document.getElementById('btn-go').innerHTML = 'Pick Up the Phone!';
+    document.getElementById("btn-go").removeEventListener("click", chooseRingingPhone);
+    document.getElementById('btn-go').addEventListener("click", ringigOff);
+}
+
+function ringigOff() {
+    document.getElementById('ringing').remove();
+    document.getElementById('btn-go').innerHTML = 'Go again :)';
+    document.getElementById('btn-go').removeEventListener("click", ringigOff);
+    document.getElementById("btn-go").addEventListener("click", chooseRingingPhone);
+}
